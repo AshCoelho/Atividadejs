f23338174da6fc4ef057d0072bbbb8eb3fd7c971
Ash Coelho <ashlakarina02@gmail.com>
2024-07-05 11:49:54 -0300

    02 questão

---
+++ exercicio.js
@@ -0,0 +1,17 @@
+
+// 01
+/*let nome = prompt("Qual o seu nome? ")
+
+const novo_nome = document.createElement('h1')
+novo_nome.innerHTML = `Seja bem Vindo(a), ${nome}!👋`
+document.querySelector('body').appendChild(novo_nome)*/
+
+// 02
+
+function ligar() {
+    document.getElementById('lampada').src = 'lampada_Acesa.jpg'
+}
+
+function desligar() {
+    document.getElementById('lampada').src = 'lampada.jpg'
+}
